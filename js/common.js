b61f59acbade5eb21df3d37ffafc7fb3b37896e5
Beam <kimjiun1028@gmail.com>
2026-02-02 09:53:27 +0900

    js 수정

--- js/common.js
+++ js/common.js
@@ -1,11 +1,13 @@
 //100vh
 function use100vh() {
-  const handleResize = () => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  };
-
   useEffect(() => {
+    const handleResize = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    handleResize();
+
     window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("resize", handleResize);
